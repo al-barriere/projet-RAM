4007f9b9a499f78315244989efc90a45a6ef8c18
Rémi <remi.momprive@gmail.com>
2017-02-06 22:18:43 +0100

    Importation photos galerie/appareil photo

--- app/src/main/java/fr/ram/traitementimage/Activities/StartActivity.java
+++ app/src/main/java/fr/ram/traitementimage/Activities/StartActivity.java
@@ -1,18 +1,34 @@
 package fr.ram.traitementimage.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+
 import fr.ram.traitementimage.R;
+import fr.ram.traitementimage.Util.ImageFile;
 
 /**
  * When you start the app, you have 2 choices :
- *  - take a picture with your camera
- *  - use a picture of your gallery
+ * - take a picture with your camera
+ * - use a picture of your gallery
  */
 
 public class StartActivity extends AppCompatActivity {
+
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int SELECT_IMAGE = 2;
+    private Uri mCurrentPhotoUri;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +37,60 @@ public class StartActivity extends AppCompatActivity {
 
         Button camera = (Button) findViewById(R.id.camera);
         Button gallery = (Button) findViewById(R.id.gallery);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+                startTreatmentActivity(mCurrentPhotoUri, true);
+            }
+        }
+        if (requestCode == SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    startTreatmentActivity(data.getData(), false);
+                }
+            }
+        }
+    }
+
+    private void dispatchSelectPictureIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = ImageFile.createImageFile(this);
+                if (photoFile != null) {
+                    mCurrentPhotoUri = FileProvider.getUriForFile(this, "fr.ram.traitementimage.fileprovider", photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCurrentPhotoUri);
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                }
+            } catch (IOException ex) {
+            }
+        }
+    }
+
+    public void onButtonGaleryClick(View view) {
+        dispatchSelectPictureIntent();
+    }
+
+    public void onButtonCameraClick(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    private void startTreatmentActivity(Uri imageUri, boolean fromCamera) {
+        Intent intent = new Intent(this, ImageTreatmentActivity.class);
+        intent.putExtra("image", imageUri);
+        intent.putExtra("fromCamera", fromCamera);
+        startActivity(intent);
     }
 }
--- app/src/main/java/fr/ram/traitementimage/Util/ImageFile.java
+++ app/src/main/java/fr/ram/traitementimage/Util/ImageFile.java
@@ -1,8 +1,94 @@
 package fr.ram.traitementimage.Util;
 
-/**
- * Created by Rémi on 05/02/2017.
- */
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ImageFile {
+    public static File createImageFile(Activity activity) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        return image;
+    }
+
+    public static Bitmap rotationImage(Uri photoUri, Bitmap bitmap) throws IOException {
+        ExifInterface exif = new ExifInterface(photoUri.getPath());
+        int exifOrientation = exif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL);
+
+        int rotate = 0;
+
+        switch (exifOrientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotate = 90;
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotate = 180;
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotate = 270;
+                break;
+        }
+
+        if (rotate != 0) {
+            int w = bitmap.getWidth();
+            int h = bitmap.getHeight();
+
+            Matrix mtx = new Matrix();
+            mtx.preRotate(rotate);
+
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            return bitmap;
+        }
+        return bitmap;
+    }
+
+    public static void getPixelsAndHistogram(Bitmap image, int[] pixels, int[][] histogram) {
+        int taille = image.getWidth() * image.getHeight();
+        int couleur[][] = new int[taille][3];
+        int tempCouleur;
+
+        if (pixels == null) {
+            pixels = new int[taille];
+        }
+        image.getPixels(pixels, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+
+        if (histogram != null) {
+            for (int i = 0; i <= 255; i++) {
+                for (int j = 0; j < 3; j++) {
+                    histogram[i][j] = 0;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < taille; i++) {
+            couleur[i][0] = Color.red(pixels[i]);
+            couleur[i][1] = Color.green(pixels[i]);
+            couleur[i][2] = Color.blue(pixels[i]);
+            for (int j = 0; j < 3; j++) {
+                tempCouleur = couleur[i][j];
+                if (histogram != null) {
+                    histogram[tempCouleur][j]++;
+                }
+            }
+        }
+
+    }
 }
+
