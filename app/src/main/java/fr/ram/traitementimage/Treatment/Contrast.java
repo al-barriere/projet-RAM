b9c0bda24f24c4c58953744c59e041509a9b33b1
Maxime Guilbault <maxime.guilbault@etu.u-bordeaux.fr>
2017-02-09 15:50:23 +0100

    sepia

---
+++ app/src/main/java/fr/ram/traitementimage/Treatment/Contrast.java
@@ -0,0 +1,18 @@
+package fr.ram.traitementimage.Treatment;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.widget.ImageView;
+
+/**
+ * Created by Maxime on 09/02/2017.
+ */
+
+public class Contrast extends Treatment {
+
+    @Override
+    public void calcul(Bitmap bmp, ImageView img)
+    {
+
+    }
+}
---
+++ app/src/main/java/fr/ram/traitementimage/Treatment/HistogramEqualization.java
@@ -0,0 +1,17 @@
+package fr.ram.traitementimage.Treatment;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.widget.ImageView;
+
+/**
+ * Created by Maxime on 09/02/2017.
+ */
+
+public class HistogramEqualization extends Treatment {
+    @Override
+    public void calcul(Bitmap bmp, ImageView img)
+    {
+
+    }
+}
