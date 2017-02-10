c650f0b2e7bf30c54e3951aac187db27af58af6f
Maxime Guilbault <maxime.guilbault@etu.u-bordeaux.fr>
2017-02-10 15:25:00 +0100

    convol

---
+++ app/src/main/java/fr/ram/traitementimage/Treatment/Convolution/Laplacien.java
@@ -0,0 +1,16 @@
+package fr.ram.traitementimage.Treatment.Convolution;
+
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+/**
+ * Created by Maxime on 10/02/2017.
+ */
+
+public class Laplacien extends Convolution{
+    @Override
+    public void calcul(Bitmap bmp, ImageView img)
+    {
+
+    }
+}
