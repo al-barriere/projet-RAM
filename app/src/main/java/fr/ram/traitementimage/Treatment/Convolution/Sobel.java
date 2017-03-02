0c46f9d3abcb98ce38f6da07ebc7e1fbe7e1379e
Maxime Guilbault <maxime.guilbault@etu.u-bordeaux.fr>
2017-03-02 17:49:54 +0100

    sobel

--- app/src/main/java/fr/ram/traitementimage/Treatment/Convolution/Sobel.java
+++ app/src/main/java/fr/ram/traitementimage/Treatment/Convolution/Sobel.java
@@ -14,7 +14,7 @@ public class Sobel extends Convolution {
     public void compute(CustomImageView img, Bundle args) {
         int maskSize = 3;
         double[][] mask = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-        double[][] mask2 = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+        double[][] mask2 = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
         args = new Bundle();
         args.putInt("nbMask",2);
