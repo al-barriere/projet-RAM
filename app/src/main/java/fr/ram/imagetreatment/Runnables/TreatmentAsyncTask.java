964dd4f8191244f069c38b06a38a501fb86e1de0
Rémi <remi.momprive@gmail.com>
2017-04-13 00:22:40 +0200

    Add comments on Runnables package

--- app/src/main/java/fr/ram/imagetreatment/Runnables/TreatmentAsyncTask.java
+++ app/src/main/java/fr/ram/imagetreatment/Runnables/TreatmentAsyncTask.java
@@ -1,6 +1,5 @@
 package fr.ram.imagetreatment.Runnables;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
@@ -21,6 +20,12 @@ public class TreatmentAsyncTask extends AsyncTask<Object, Integer, Bitmap> {
     private ProgressDialog progressDialog;
     private ImageTreatmentActivity delegate;
 
+    /***
+     * Compute the treatment on the Bitmap
+     * @param params Arguments for the process including the CustomImageView, the Treatment arguments,
+     *               the Treatment class, the calling activity and the effect ProgressDialog
+     * @return The modified bitmap
+     */
     @Override
     protected Bitmap doInBackground(Object... params) {
         this.img = (CustomImageView) params[0];
@@ -29,9 +34,15 @@ public class TreatmentAsyncTask extends AsyncTask<Object, Integer, Bitmap> {
         this.delegate = (ImageTreatmentActivity) params[3];
         this.progressDialog = (ProgressDialog) params[4];
 
+        // Compute the treatment on the CustomImageView
         return treatment._compute(img, args);
     }
 
+    /***
+     * Inform the calling activity that the execution is finished
+     * @param result The modified bitmap
+     */
+    @Override
     protected void onPostExecute(Bitmap result) {
         delegate.processFinish(result, progressDialog);
     }
