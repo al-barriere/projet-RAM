0ed6a3cfe58ae4dc98fe5d98395291b88223eafd
remimomprive <rm.mc@hotmail.fr>
2017-04-18 21:41:48 +0200

    Code comments

--- app/src/main/java/fr/ram/imagetreatment/Treatments/Treatment.java
+++ app/src/main/java/fr/ram/imagetreatment/Treatments/Treatment.java
@@ -14,12 +14,25 @@ import fr.ram.imagetreatment.Runnables.TreatmentAsyncTask;
  * Created by Maxime on 03/02/2017.
  */
 public abstract class Treatment {
+    /***
+     * This method contains the logic of the Treatment
+     * @param bmp The Bitmap to modify
+     * @param args The arguments of the Treatment
+     * @return The modified Bitmap
+     */
     public abstract Bitmap _compute(Bitmap bmp, Bundle args);
 
+    /***
+     * This method is called by the ImageTreatmentActivity in order to start a Treatment
+     * @param activity The calling activity
+     * @param img The CustomImageView containing the Bitmap to modify
+     * @param args The arguments of the Treatment
+     */
     public void compute(Activity activity, CustomImageView img, Bundle args) {
+        // Start a ProgressDialog in order to avoid the feeling of a blocked UI
         ProgressDialog effectProgressDialog = ProgressDialog.show(activity, activity.getString(R.string.please_wait), activity.getString(R.string.processing_image), true);
         effectProgressDialog.setCancelable(false);
-
+        // Create and execute the treatment in another thread
         TreatmentAsyncTask treatmentRunnable = new TreatmentAsyncTask();
         treatmentRunnable.execute(img.getImageBitmap(), args, this, activity, effectProgressDialog);
     }
